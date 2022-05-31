d6d6d796c46dd53f051954bc9a27dd530ea735b0
AlexD70 <alex.do.mihai@gmail.com>
2022-05-31 22:14:56 +0300

    check bugs in todo.txt

--- classes.cpp
+++ classes.cpp
@@ -1,8 +1,39 @@
+#include <string>
+
+enum TokenType{
+    WHITESPACE,             //lasa-l ca poate ne ajuta mai trz
+    IDENTIFIER,             //asta va fi string initial
+    INTEGER,                //0 1 2 3 4 5 6 7 8 9 
+    STRING,                 //kdjgfkd   
+    OPERATOR,               //+ - / % * = < > <= >= 
+    SEPARATOR,              //,
+    PARANTHESES,            // ()
+    INDENTATION,            // \n tab (\n 4 spaces) 
+    VALUE_ASSIGNEMENT,      //<-
+    KEYWORD,
+    OPERATOR__,             //special wrapping for -
+    SUBSCRIPT,              // []
+    DEFAULT
+};
+
 class Token{
     public:
         char* ptr;
         int size = 0; //size = real_size - 1
+        enum TokenType tokenType;
+        std::string self = "";
+
+        void buildString(){
+            for (int i = 0; i <= size; i++){
+			    self.push_back(*(ptr + i));
+			}
+        }
+
+		int __repr__(){
+			return tokenType;
+		}
 };
+
 /*
 tab: 4spaces \n4space
 string
@@ -13,3 +44,6 @@ nr
 <-
 //
 /* */
+
+
+//reminder noi n am fc nimic pt numerele negative, dar deocamdata pastram doar nr naturale 
--- parser.cpp
+++ parser.cpp
@@ -1,127 +1,253 @@
 #include <fstream>
 #include <iostream>
-//#include <filesystem>
 #include "classes.cpp"
-#include <string>
 #include <vector>
 
 int main() {
+    /*
+    ** READ FROM filename INTO filebuffer
+    */
+
     std::vector<char> filebuffer;
     std::string filename;
     
 
     std::cin >> filename;
-    // int k = std::filesystem::file_size(filename);
-
     std::ifstream f(filename);
     int k = 0;
-    // char* filebuffer = new char[k];
     
-    for (int i = 0; !(f.eof()); i++){
+    for (int i = 0; ; i++){
+        char ch;
         filebuffer.reserve(i + 2);
-        filebuffer.push_back(f.get());
-        std::cout << filebuffer[i];
+
+        ch = f.get();
+        if (f.eof()){
+            break;
+        }
+
+        filebuffer.push_back(ch);
+        //std::cout << filebuffer[i];
         k = i;
     }
 
-    std::cout << "\n\n";
+    //std::cout << "\n\n";
+
+
+    /*
+    ** CREATE TOKENS
+    */
 
     char* ptr = nullptr;
     int size = 0;
-    Token* tokens = new Token[k];
+    bool inString = false;
+    char stringType;
+    bool inParantheses = false;
+    int paranthesesDepth = 0;
+    
+    std::vector<Token> tokens;
+    tokens.reserve(k);
+
     int j = 0;
 
     for (int i = 0; i < k; i++){
-        if (filebuffer[i] == '+'){
+
+        /*
+        ** OPERATORS
+        */
+
+       char ref = filebuffer[i];
+
+       while (inString){
+           if ((filebuffer[i] == stringType) && (filebuffer[i - 1] != '\\')){
+                inString = false;
+           }
+           if (i == k - 1){
+               std::cout << "SyntaxError: string literal reaches EOF.\n";
+               exit(-1);
+           }
+           if (filebuffer[i] == '\n'){
+               std::cout << "SyntaxError: multi-line string literals not allowed.\n";
+               exit(-1);
+           }
+           tokens[j - 1].size += 1;
+           i += 1;
+       }
+
+        if (ref == '+'){  //ADDITION
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = OPERATOR; 
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == '-'){
+        } else if (ref == '-'){  //SPECIAL WRAPPER FOR MINUS SIGN
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = OPERATOR__; 
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == '*'){
+        } else if (ref == '*'){  //MULTIPLY
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = OPERATOR;
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == '%'){
+        } else if (ref == '%'){  //MODULUS
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = OPERATOR;
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == '='){
+        } else if (ref == '='){  //EQUALITY (NON STRICT)
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = OPERATOR;
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == ','){
+        } else if (ref == '>'){  //GT and GTorEQ
             tokens[j].ptr = &filebuffer[i];
-            j += 1;
-            continue;
-
-        } else if (filebuffer[i] == '>'){
-            tokens[j].ptr = &filebuffer[i];
-            if (filebuffer[j + 1] == '='){
+            if (filebuffer[i + 1] == '='){
                 tokens[j].size += 1;
+                tokens[j].tokenType = OPERATOR;
                 i += 1;
             }
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == '<'){
+        } else if (ref == '<'){  //LT and LTorEQ
             tokens[j].ptr = &filebuffer[i];
-            if (filebuffer[j + 1] == '='){
+            if (filebuffer[i + 1] == '='){
                 tokens[j].size += 1;
+                tokens[j].tokenType = OPERATOR;
                 i += 1;
-            } else if (filebuffer[j + 1] == '-'){
+
+                /*
+                ** ASSIGNMENT
+                */
+
+            } else if (filebuffer[i + 1] == '-'){
                 tokens[j].size += 1;
+                tokens[j].tokenType = VALUE_ASSIGNEMENT;
                 i += 1;
             }
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == '\''){
+        /*
+        ** STRINGS
+        */
+
+        } else if (ref == '\''){
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = STRING;
+            j += 1;
+            inString = true;
+            stringType = '\'';
+            continue;
+
+        } else if (ref == '\"'){
+            tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = STRING;
+            j += 1;
+            inString = true;
+            stringType = '\"';
+            continue;
+
+        /*
+        ** SEPARATORS
+        */
+
+        } else if (ref == ','){
+            tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = SEPARATOR;
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == '\"'){
+        /*
+        ** PARANTHESES
+        */
+
+        } else if (ref == '('){
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = PARANTHESES;
+            inParantheses = true;
+            paranthesesDepth += 1;
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == '('){
+        } else if (ref == ')'){
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = PARANTHESES;
+            paranthesesDepth -= 1;
+            if (paranthesesDepth == 0){
+                inParantheses = false;
+            } else if (paranthesesDepth < 0){
+                std::cout << "SyntaxError: bad parantheses nesting.\n";
+                exit(-1);
+            }
             j += 1;
             continue;
 
-        } else if (filebuffer[i] == ')'){
-            tokens[j].ptr = &filebuffer[i];
-            j += 1;
-            continue;
+        /*
+        ** COMMENTS - literally skips them
+        */
 
-        } else if ((filebuffer[i] == '/') && (filebuffer[i + 1] == '/')){
+        } else if ((ref == '/') && (filebuffer[i + 1] == '/')){
             do{
                 i += 1;
             } while (filebuffer[i] != '\n');
             continue;
 
-        } else if ((filebuffer[i] == '/') && (filebuffer[i + 1] == '*')){
+        } else if ((ref == '/') && (filebuffer[i + 1] == '*')){
             do{
                 i += 1;
+                if (i == k - 2){
+                    std::cout << "SyntaxError: multi-line comment reches EOF.\n";
+                    exit(-1);
+                } 
+
             } while ((filebuffer[i] != '/')&&(filebuffer[i + 1] != '*'));
             i += 1;
             continue;
 
-        } else if (filebuffer[i] == '/'){
+        /*
+        ** DEFAULT - maps later to IDENTIFIER or KEYWORD
+        */
+
+        } else if (isalpha(ref) || ref == '_'){
             tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = DEFAULT;
+
+            while(true){
+                i += 1;
+                if (!((isalnum(filebuffer[i])) || (filebuffer[i] == '_'))){
+                    break;
+                }
+                tokens[j].size += 1;
+            } 
+
             j += 1;
             continue;
 
+        /*
+        ** INTEGER LITERALS
+        */
+
+        } else if (isdigit(ref)){
+            tokens[j].ptr = &filebuffer[i];
+            tokens[j].tokenType = INTEGER;
+
+            while((filebuffer[i+1] > 64 && filebuffer[i+1] < 91) ||
+                (filebuffer[i+1] > 96 && filebuffer[i+1] < 123)) {
+                    tokens[j].size += 1;
+                    i += 1;
+            }
+            continue;
         }
+    }
+
+    if (inParantheses){
+        std::cout << "SyntaxError: bad parantheses nesting.\n";
+        exit(-1);
     }
 
 
@@ -129,7 +255,5 @@ int main() {
         std::cout << *(tokens[i].ptr) << '\n';
     }
 
-    std::cout << j;
-
-    delete[] tokens;
+    std::cout << j << '\n';
 }
